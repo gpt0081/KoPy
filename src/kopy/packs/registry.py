7c19cc133bae06d946caaa1a2b0642c074c27699
gpt0081 <131793254+gpt0081@users.noreply.github.com>
2026-08-25 01:53:54 +0900

    feat: register Matplotlib pack

--- src/kopy/packs/registry.py
+++ src/kopy/packs/registry.py
@@ -9,6 +9,7 @@ from typing import Any
 from .accelerate import ACCELERATE_PACK
 from .base import LibraryPack, PackMemberInfo
 from .datasets import DATASETS_PACK
+from .matplotlib import MATPLOTLIB_PACK
 from .mlflow import MLFLOW_PACK
 from .numpy import NUMPY_PACK
 from .onnxruntime import ONNXRUNTIME_PACK
@@ -26,7 +27,7 @@ _BUILTIN_PACKS: tuple[LibraryPack, ...] = (
     NUMPY_PACK, PANDAS_PACK, SKLEARN_PACK, TORCH_PACK, TRANSFORMERS_PACK,
     DATASETS_PACK, TOKENIZERS_PACK, ACCELERATE_PACK, PEFT_PACK,
     ONNXRUNTIME_PACK, SAFETENSORS_PACK, OPTIMUM_PACK, SENTENCEPIECE_PACK,
-    MLFLOW_PACK,
+    MLFLOW_PACK, MATPLOTLIB_PACK,
 )
 
 
